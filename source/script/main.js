3cc8507d4d1ec8b65095d53dea2b2d1595e85f59
Léo Campos <techmuha@gmail.com>
2016-08-20 21:01:18 -0300

    Diagramação -> Continuando diagramação
    off-canvas e finalizando home-page

--- source/script/main.js
+++ source/script/main.js
@@ -1,2 +1,39 @@
 $(document).ready(function() {
+	// TOGGLE BUTTON
+	$('[data-class="toggle"]').click(function(){
+		$(this).toggleClass('checked')
+	});
+
+
+	// OFFCANVAS
+	function openOffcanvas($element) {
+		$element.addClass('open-offcanvas');
+		$('html, body').addClass('no-overflow');
+	}
+
+	function hideOffcanvas($element) {
+		$element.removeClass('open-offcanvas');
+		$('html, body').removeClass('no-overflow');
+	}
+
+	$('[data-class="openmenu"]').on('click', function(){
+		openOffcanvas($('html'));
+	});
+
+	$('html, body').on("click", function(e){
+		if(this === e.target)
+		hideOffcanvas($(this));
+	});
+
+
+	// DETECTANDO LARGURA DA VIEWPORT PARA REMOVER SWIPE
+	if (jQuery(window).width() < 992) {
+		$('html, body').on("swiperight", function(){
+			openOffcanvas($('html'));
+		});
+
+		$('html, body').on("swipeleft", function(){
+			hideOffcanvas($('html'));
+		});
+	};
 });
